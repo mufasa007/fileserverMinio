232b23382f1f74563d88ad17af96196c6a224907
wanyu <594564264@qq.com>
2022-02-14 22:12:57 +0800

    [INIT] 新增项目

---
+++ src/main/java/com/activeclub/fileserverminio/FileserverMinioApplication.java
@@ -0,0 +1,13 @@
+package com.activeclub.fileserverminio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileserverMinioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileserverMinioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/activeclub/fileserverminio/FileserverMinioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.activeclub.fileserverminio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileserverMinioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
