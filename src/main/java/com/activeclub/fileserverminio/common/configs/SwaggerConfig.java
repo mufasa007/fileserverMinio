3843bda5c1f0d4b41ebdd2d26f4dabe56ecdc73d
wanyu <594564264@qq.com>
2022-02-14 22:59:44 +0800

    [ADD]:swagger-ui

--- src/main/java/com/activeclub/fileserverminio/common/configs/SwaggerConfig.java
+++ src/main/java/com/activeclub/fileserverminio/common/configs/SwaggerConfig.java
@@ -1,10 +1,58 @@
-//package com.activeclub.fileserverminio.common.configs;
-//
-//import org.springframework.context.annotation.Configuration;
-//import springfox.documentation.swagger2.annotations.EnableSwagger2;
-//
-//@Configuration //说明这是一个配置类
-//@EnableSwagger2// 该注解开启Swagger2的自动配置
-//public class SwaggerConfig {  //随便的一个类名
-//
-//}
+package com.activeclub.fileserverminio.common.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+//@ComponentScan("com.activeclub.*")
+@EnableSwagger2
+public class SwaggerConfig extends WebMvcConfigurationSupport {
+    @Bean
+    public Docket createDocket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(getApiInfo())  //api信息
+                .select()   //扫描哪个路径下的哪些API
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    /**
+     * swagger页面中显示相关信息
+     *
+     * @return 作者信息
+     */
+    private ApiInfo getApiInfo() {
+        return new ApiInfoBuilder()
+                .title("文件管理服务")
+                .contact(new Contact("wanyu", "https://www.cnblogs.com/Mufasa/", "xxx@qq.com"))
+                .version("1.0.0")
+                .build();
+    }
+
+    /**
+     * 添加静态文件路由
+     * @param registry 注册表(登记)
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations(
+                "classpath:/static/");
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations(
+                "classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations(
+                "classpath:/META-INF/resources/webjars/");
+        super.addResourceHandlers(registry);
+    }
+
+}
